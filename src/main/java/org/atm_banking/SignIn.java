b760408d736064ae2a4795eec70e9a771ecc0aed
gitongaisaac <isaacggitonga@gmail.com>
2023-08-01 02:12:47 +0300

    Sign validation using database

--- src/main/java/org/atm_banking/SignIn.java
+++ src/main/java/org/atm_banking/SignIn.java
@@ -3,10 +3,11 @@ package org.atm_banking;
 /* Import packages */
 import java.util.Scanner;
 import org.db_banking.DbNonParamAccessors;
+import org.db_banking.DbParamAccessors;
 
 /* This class takes care of user validation to ensure that users only access accounts to which they have access to.
  * Has 2 methods. activity() and signIn()  */
-class SignIn extends DbNonParamAccessors {
+class SignIn extends DbParamAccessors {
 
     /* This method Displays the activities that the user can perform on their account after they sign in and are validated.
      * The user performs a specific activity by entering the number corresponding to the activity they wish to access */
@@ -29,25 +30,25 @@ class SignIn extends DbNonParamAccessors {
     public static void signIn() {
         int accountNo = 9804;
         int pinNo = 9804;
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Account number
         System.out.print("Account Number: ");
         int enteredNo = scanner.nextInt();
 
-        String query = "SELECT account_pin FROM Account_info WHERE account_no = enteredNo";
+        String query = "SELECT account_pin FROM Account_info WHERE account_no = ?";
+        int condition = enteredNo;
+        String label = "account_pin";
 
         SignIn signIn = new SignIn();
-        signIn.dbIntegerAccessor(query);
+        int result = signIn.dbIntAccessor(query, condition, label);
 
         // PIn
         System.out.print("Pin number: ");
         int enteredPin = scanner.nextInt();
 
-        /* Validation. Checks if the Account Number entered corresponds to the pin. It gives the user 3 chances to enter
-         * the credentials */
-        if (enteredNo != accountNo & enteredPin != pinNo) {
-            for (int i = 1; i < 3; i++ ) {
+        if (enteredPin != result) {
+            for (int i = 1; i < 3; i++) {
                 System.out.println("The details do not match");
 
                 System.out.print("Account Number: ");
@@ -56,21 +57,36 @@ class SignIn extends DbNonParamAccessors {
                 System.out.print("Pin number: ");
                 enteredPin = scanner.nextInt();
             }
+        } else {
+            activity();
         }
 
-        else
-            activity();
-
+        /* Validation. Checks if the Account Number entered corresponds to the pin. It gives the user 3 chances to enter
+         * the credentials */
+//        if (enteredNo != accountNo & enteredPin != pinNo) {
+//            for (int i = 1; i < 3; i++) {
+//                System.out.println("The details do not match");
+//
+//                System.out.print("Account Number: ");
+//                enteredNo = scanner.nextInt();
+//
+//                System.out.print("Pin number: ");
+//                enteredPin = scanner.nextInt();
+//            }
+//        } else {
+//            activity();
+//        }
     }
 
     /* *********************************************************************************************************** */
-    public static void main(String[] args) {
-        String SSN = "096-43-2509";
-        String query = "SELECT last_name FROM Person_info WHERE SSN = SSN";
-
-        SignIn signIn = new SignIn();
-        String result = signIn.dbStringAccessor(query);
-
-        System.out.println(result);
-    }
+//    public static void main(String[] args) {
+//        String query = "SELECT account_pin FROM Account_info WHERE account_no = ?";
+//        int condition = 123456789;
+//        String label = "account_pin";
+//
+//        SignIn signIn = new SignIn();
+//        int result = signIn.dbIntAccessor(query, condition, label);
+//
+//        System.out.println(result);
+//    }
 }
